cc62d83f5f832f1e5ca4db14eb97a52e2c8e1387
Raul Sanchez <raulenrique6@hotmail.com>
2021-04-25 17:15:39 -0400

    Integration Layer Initial Commit

--- src/Integration/integrationLayer.js
+++ src/Integration/integrationLayer.js
@@ -1,2 +1,16 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('app.db');
+
+const { ipcMain } = require('electron');
+const sqlite3 = require('sqlite3');
+
+const database = new sqlite3.Database('./public/db.sqlite3', (err) => {
+    if (err) console.error('Database opening error: ', err);
+});
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+    const sql = arg;
+    database.all(sql, (err, rows) => {
+        event.reply('asynchronous-reply', (err && err.message) || rows);
+    });
+});
--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 660,
     height: 660,
-    frame: false
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+    }
   });
 
   // and load the index.html of the app.
