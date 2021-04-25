16931cbd0c394bbbf42f6c63cfa60e62c43e8b14
manuel41 <medmundogonzalez@gmail.com>
2021-04-25 15:03:01 -0400

    Commit with previous app changes

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 660,
+    height: 720,
   });
 
   // and load the index.html of the app.
+  mainWindow.setResizable(false);
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
