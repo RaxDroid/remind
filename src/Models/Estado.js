11a7c6e86e57609b5655ec77fab93d0e78f37464
Raul Sanchez <raulenrique6@hotmail.com>
2021-04-25 21:02:51 -0400

    Added Models

---
+++ src/Models/Estado.js
@@ -0,0 +1,16 @@
+//  Constants
+
+let estadoTypes = {
+    "Reminder": 1,
+    "Materia": 2
+}
+
+//  Logic
+
+class Estado{
+    constructor(id, descripcion, tipoEstado){
+        this.id = id;
+        this.descripcion = descripcion;
+        this.tipoEstado = tipoEstado;
+    }
+}
